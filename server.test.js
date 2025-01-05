6072dc4b60ab831e60e0d82201fdadc0eefdb1f1
Mujahid <mujahidramdhani@gmail.com>
2025-01-06 06:19:08 +0700

    add youtube endpoint
    Signed-off-by: Mujahid <mujahidramdhani@gmail.com>

--- server.test.js
+++ server.test.js
@@ -2,13 +2,13 @@ const app = require('./server') // Link to your server file
 const supertest = require('supertest')
 const request = supertest(app)
 
-
-it('Call the /youtube endpoint', async done => {
+it('Call the / endpoint', async done => {
     const res = await request.get('/youtube')
     expect(res.status).toBe(200)
     expect(res.text).toBe('Hello, youtube indonesia!')
     done()
 })
+
 it('Call the / endpoint', async done => {
     const res = await request.get('/')
     expect(res.status).toBe(200)
