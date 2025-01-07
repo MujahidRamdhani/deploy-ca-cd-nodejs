2b98bc416ca9aec745a90714b47b77916fcd939c
Mujahid <mujahidramdhani@gmail.com>
2025-01-07 09:19:55 +0700

    tambah youtube
    Signed-off-by: Mujahid <mujahidramdhani@gmail.com>

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/instagram', (req,res) => res.send('Hello, instagram indonesia dan kota bandung!'))
 app.get('/reddit', (req,res) => res.send('Hello, reddit indonesia dan kota bandung!'))
+app.get('/youtube', (req,res) => res.send('Hello, youtube indonesia dan kota bandung!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
 })
